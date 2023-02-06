d19df64176e67bb8446c81e5be5cab2b8343ef2b
dotsch <dominik.poeschel@hotmail.com>
2023-02-06 20:12:12 +0100

    serial Test: verschiedene Sende raten moeglich, tare problem noch, es kann zu negativen Gewichten kommen

--- serialtest.py
+++ serialtest.py
@@ -1,24 +1,31 @@
 import serial
 from binascii import hexlify
 from time import sleep
-try:
+
+def starte_port():
     serialPort = serial.Serial('/dev/ttyS0')
-except:
-    tt = subprocess.Popen(["chmod", "666", "/dev/ttyS0"], stdout=subprocess.PIPE)
-    serialPort = serial.Serial('/dev/ttyACM0')
+    return serialPort
 
-def set_sending_speed():
+
+def set_sending_speed_500(port):
     data = "02 30 30 30 31 57 30 30 31 33 30 33 35 30 30 36 32 03"
-    send_data(data)
+    send_data(data,port)
 
 
-#def start_sending():
+def set_sending_speed_50(port):
+    data = "02 30 30 30 31 57 30 30 31 33 30 32 35 30 36 32 03"
+    send_data(data,port)
 
 
 
-def stop_sending():
+def start_sending(port):
+    data = "02 30 30 30 31 45 31 30 31 31 30 30"
+    send_data(data,port)
+
+def stop_sending(port):
     data =  "02 30 30 30 31 45 31 30 31 30 30 30"
-    send_data(data)
+    send_data(data,port)
+
 
 
 def calc_lrc(data_in_bytes):
@@ -30,7 +37,7 @@ def calc_lrc(data_in_bytes):
     return lrc
 
 
-def send_data(data):
+def send_data(data,serialPort):
     data_in_bytes  = bytearray.fromhex(data)
     lrc = calc_lrc(data_in_bytes)
     for c in lrc:
@@ -40,55 +47,52 @@ def send_data(data):
     data_in_bytes.extend(x)
     serialPort.write(data_in_bytes)
 
-#data = "02 31 37 30 31 52 30 31 30 31 30 30"
-#ata = "02 30 30 30 31 52 30 31 30 37 30 30 30 30 03"
-data = "02 30 30 30 31 45 31 30 31 31 30 30"
-data_in_bytes  = bytearray.fromhex(data)
-print(data_in_bytes)
-#data_in_bytes  = bytes.fromhex(data)
 
-lrc = 0
-for b in data_in_bytes[1:]:
-    
-    lrc ^= b
-    print(lrc)
+def read_gewicht(serialSubstring):
+    c = 0
+    for count, s in enumerate(serialSubstring):
+        s=chr(s) 
+        print(s)
+        if s == ' ':
+            s = '0'
 
-print(lrc)
-print(lrc.bit_length())
-print(data_in_bytes)
-lrc = str(lrc)
-for c in lrc:
-    data_in_bytes.extend(c.encode())
+        if count == 2:
+            c += int(s) * 10
 
-print(lrc)
+        if count == 3:
+            c += int(s)
+        if count == 5:
+            c += int(s) * 0.1
+        if count == 6:
+            c += int(s) * 0.01
+        if count == 7:
+            c += int(s) * 0.001
 
-print(len(lrc))
-stop_sending()
+    gewicht = c
+    print(gewicht)
 
-#data_in_bytes.append(lrc)
-x = bytes.fromhex('03')
-data_in_bytes.extend(x)
-print(data_in_bytes.hex())
-#data_in_bytes=bytearray.fromhex(data_in_bytes.hex())
-print(data_in_bytes)
-print(len(data_in_bytes))
-for b in data_in_bytes:
-    print(chr(b))
 
-serialPort.write(data_in_bytes)
-try:
-    while 1:
-  #  serialPort.write(data_in_bytes)
-        serialString = []
-        s_String = []
-        s_String = serialPort.readline()
-        print(s_String)
-        print(s_String[12:22])
+if __name__ == '__main__':
+    serialPort = starte_port()
 
-        sleep(1)
-except KeyboardInterrupt:
-   print("Program terminated manually!")
-   stop_sending()
-   raise SystemExit
+    try:
+        set_sending_speed_500(serialPort)
+        start_sending(serialPort)
+        while 1:
+            serialString = []
+            s_String = []
+            s_String = serialPort.readline()
+            print(s_String)
+            serialSubstring=s_String[13:21]
+            print(serialSubstring)
+            try:
+                read_gewicht(serialSubstring)
+            except:
+                pass
+    except KeyboardInterrupt:
+        print("Program terminated manually!")
+        stop_sending(serialPort)
+        raise SystemExit
+
 
 
