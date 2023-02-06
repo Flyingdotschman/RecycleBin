07616069dd84494cc31f0d5c701fdc8912ebc9cb
dotsch <dominik.poeschel@hotmail.com>
2023-02-06 12:33:21 +0100

    serial Test Gewicht abfragen das erste Mal erfolgreich

--- serialtest.py
+++ serialtest.py
@@ -1,4 +1,5 @@
 import serial
+from binascii import hexlify
 
 try:
     serialPort = serial.Serial('/dev/ttyS0')
@@ -7,31 +8,50 @@ except:
         serialPort = serial.Serial('/dev/ttyACM0')
 
 data = "02 31 37 30 31 52 30 31 30 31 30 30"
+ata = "02 30 30 30 31 52 30 31 30 37 30 30 30 30 03"
 data_in_bytes  = bytearray.fromhex(data)
 print(data_in_bytes)
 #data_in_bytes  = bytes.fromhex(data)
 
 lrc = 0
 for b in data_in_bytes[1:]:
+    
     lrc ^= b
+    print(lrc)
+
 print(lrc)
 print(lrc.bit_length())
 print(data_in_bytes)
-data_in_bytes.append(lrc)
+lrc = str(lrc)
+for c in lrc:
+    data_in_bytes.extend(c.encode())
+
+print(lrc)
+
+print(len(lrc))
+
+
+#data_in_bytes.append(lrc)
 x = bytes.fromhex('03')
 data_in_bytes.extend(x)
 print(data_in_bytes.hex())
 data_in_bytes=bytearray.fromhex(data_in_bytes.hex())
 print(data_in_bytes)
+print(len(data_in_bytes))
+for b in data_in_bytes:
+    print(chr(b))
+
+#serialPort.write(bytearray.fromhex(ata))
 serialPort.write(data_in_bytes)
 serialString = []
-
-while 1:
+s_String = []
+while s_String!=b'\r':
     
     s_String = serialPort.read()
     
 
     serialString += s_String.decode('Ascii')
     s_String = serialPort.read(1)
+    print(str(s_String))
 print(serialString)    # Print the contents of the serial data
  
